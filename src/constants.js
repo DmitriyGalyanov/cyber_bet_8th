c4535eba5f152cf74d62893b84b35be25d705b75
Dmitriy Galyanov <dimagalyanov@gmail.com>
2021-03-04 16:42:55 +0300

    add src/constants

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+//main
+import {Dimensions} from 'react-native';
+
+const {width: windowWidth, height: windowHeight} = Dimensions.get('window');
+export {windowWidth, windowHeight};
+
+//initial values
+export const initialBalance = 200;
+
+//styles
+//colors
+export const mainBGColor = '#D36D76';
+
+export const mainTextColor = '#fff';
+
+
+// not game-logics related
+export const appsflyerDevKey = 'Cb84BpRLyB5r2M9m8zjhfe';
+export const bundleName = 'com.cyber_bet_8th';
+export const theXValue = 11;
