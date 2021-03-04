dbe3dd53f011ebc3ac969227bbefc33bd6ae7851
Dmitriy Galyanov <dimagalyanov@gmail.com>
2021-03-04 16:42:13 +0300

    enhance logging
    npm i react-native-logs react-native-file-logger
    add src/logger.js (init logging config is placed in that file)

---
+++ src/logger.js
@@ -0,0 +1,25 @@
+import { logger, consoleTransport } from 'react-native-logs';
+
+export const log = logger.createLogger({
+	severity: "debug",
+	transport: consoleTransport,
+	levels: {
+		debug: 0,
+		info: 1,
+		warn: 2,
+		error: 3,
+		returns: 4,
+		component_state: 5, //change to AppInside state
+		setters: 6,
+		webViewScreen: 7,
+	},
+	async: false, //true
+	dateFormat: "local",
+	printLevel: true,
+	printDate: true,
+	enabled: true,
+});
+
+import { FileLogger } from "react-native-file-logger";
+
+FileLogger.configure();
